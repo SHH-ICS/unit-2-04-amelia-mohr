2d135f6e64f20cac18824f2c69c45f8c4258c8e4
amelia-mohr <124377942+amelia-mohr@users.noreply.github.com>
2023-03-09 14:36:40 +0000

    fix

--- program.py
+++ program.py
@@ -6,10 +6,7 @@
 # For multiples of 15, output   FizzBuzz
 # End at 32
 
-x = int(input("Start: "))
-y = int(input("to Finish: "))
-y = y + 1
-for numbers in range(x,y): 
+for numbers in range(1,33): 
     if numbers == 0.0:
         print(str(numbers) + " ZERO!!!")
     elif numbers % 15 == 0.0:
